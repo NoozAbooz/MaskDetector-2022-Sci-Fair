c7b7ae94ea701c4d90801ff39ff480f4d7c3eed4
RPI News <zhengm58@gmail.com>
2022-04-13 20:36:36 -0600

    Rebase code

---
+++ MaskDetectorVideo.py
@@ -0,0 +1,197 @@
+# Small disclaimer
+print("[INFO] Starting program... (might take a while to load)")
+
+# Import stuff
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import threading
+import time
+import cv2
+import os
+import paramiko
+
+# Define important variables
+faceDetector = "Face Detector"
+model = "MaskDetectorV2.model"
+minConfidence = 0.7
+timeoutPeriod = 5
+disableEV3 = True
+
+start_time = time.time()
+display_time = 2
+fc = 0
+FPS = 0
+
+# Establish EV3 connection via SSH
+host = 'ev3dev.local'
+username = 'robot'
+password = 'pihole'
+con = paramiko.SSHClient()
+con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+if disableEV3 == False:
+    # Establish SSH connection
+    con.connect(hostname=host, username=username, password=password)
+    print("[INFO] Connected to EV3")
+inTimeout = False
+
+''' Load models '''
+
+# Load the face detector model (https://github.com/gopinath-balu/computer_vision/tree/master/CAFFE_DNN)
+print("[INFO] Loading face detector model...")
+prototxtPath = faceDetector + "\\deploy.prototxt"
+weightsPath = faceDetector + "\\facedetector.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# Load the face mask detector model
+print("[INFO] Loading mask model...")
+maskNet = load_model(model)
+
+''' Define main functions '''
+
+def EV3Timeout():
+    global inTimeout
+    inTimeout = True
+    time.sleep(timeoutPeriod)
+    inTimeout = False
+
+def EV3Caller(maskless):
+    # Send mask total count to EV3
+    if not inTimeout:
+        if disableEV3 == False:
+            print("[INFO] Calling EV3 Script")
+            timer = threading.Thread(target=EV3Timeout)
+            con.exec_command('~/EV3Code/DispenseV2.sh ' + str(maskless))
+            timer.start()
+
+def FaceDetector(frame, faceNet):
+    global detections	
+
+    # Grab webcam size and then construct a blob (group of connected pixels in an image that share some common property https://learnopencv.com/blob-detection-using-opencv-python-c/)
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+        (104.0, 177.0, 123.0))
+
+    # Pass the blob through face detector
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+# Main mask detection function
+def detect_and_predict_mask(frame, maskNet):
+    (h, w) = frame.shape[:2]
+    FaceDetector(frame, faceNet)
+
+    # Init list of faces, their corresponding locations, and the list of predictions from face detector
+    faces = []
+    locs = []
+    preds = []
+
+    # Loop over each face detection
+    for i in range(0, detections.shape[2]):
+        
+        # Extract the confidence associated with the detection
+        confidence = detections[0, 0, i, 2]
+
+        # Filter out weak detections
+        if confidence > minConfidence:
+            
+            # Compute the (x, y) coordinates of the bounding box for the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # Ensure the bounding boxes fall within the dimensions of the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # Extract the face return value, convert it from BGR to RGB ordering, resize to 244x244 and process it
+            face = frame[startY:endY, startX:endX]
+            if face.any():
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+
+                # Add the face and bounding boxes to their respective dicts
+                faces.append(face)
+                locs.append((startX, startY, endX, endY))
+
+    # Only make a mask prediction if at least one face was detected
+    if len(faces) > 0:
+        # Process all faces instead of one at a time for speeeed
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=20)
+
+    # Return face locations and their corresponding locations
+    return (locs, preds)
+
+# Initialize the video stream
+print("[INFO] Ininializing video...")
+vs = VideoStream(src=0).start()
+
+# Loop over each frame from video
+while True:
+    # Resize to 600x400
+    frame = vs.read()
+    frame = imutils.resize(frame, width=600, height=400)
+
+    # Run the faces through face/mask detector and log return value
+    (locs, preds) = detect_and_predict_mask(frame, maskNet)
+
+    fc+=1
+    TIME = time.time() - start_time
+
+    if (TIME) >= display_time :
+        FPS = fc / (TIME)
+        fc = 0
+        start_time = time.time()
+
+    fps_disp = "FPS: "+str(FPS)[:5]
+
+    # Loop over each face
+    maskless = 0
+    for (box, pred) in zip(locs, preds):
+        
+        # Unpack bounding box positions and labels
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+
+        # Set colours for boxes
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # Add to number of people not wearing masks
+        if label == "No Mask":
+            maskless += 1
+
+        # Add probability to box
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+        # Render boxes on faces
+        cv2.putText(frame, label, (startX, endY + 20),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    print("People not wearing masks: " + str(maskless))
+
+    cv2.putText(frame, fps_disp, (10, 25),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (230, 217, 67), 2)
+
+    # Show final frame with the boxes
+    cv2.imshow("Mask Detector", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # Call the EV3 code if 1 or more people aren't wearing masks
+    if maskless > 0:
+        EV3Caller(maskless)
+        maskless = 0
+    
+    # Close window if `Q` is pressed
+    if key == ord("q"):
+        break
+
+# Cleanup window and stop webcam feed
+cv2.destroyAllWindows()
+vs.stop()
---
+++ Train.py
@@ -0,0 +1,131 @@
+# Haha CPU go brrrrrrrrrrrrrrrrrrrrr
+
+'''
+python train_mask_detector.py ^
+--dataset Dataset --plot PlotV2.png --model MaskDetectorV2.model
+'''
+
+# Gotta import them all!
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
+
+# Take arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True)
+ap.add_argument("-m", "--model", type=str, default="mask_detector2.model")
+args = vars(ap.parse_args())
+
+# Define main training paramaters
+INIT_LR = 1e-4
+EPOCHS = 25
+BS = 10
+
+# Get dict of dataset paths and init other needed dicts
+print("Loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+data = []
+labels = []
+
+# Pre-process every image
+for imagePath in imagePaths:
+	label = imagePath.split(os.path.sep)[-2]
+
+	# Resize image (224x224) and convert to array
+	image = load_img(imagePath, target_size=(224, 224))
+	image = img_to_array(image)
+	image = preprocess_input(image)
+
+	# Add array to dict
+	data.append(image)
+	labels.append(label)
+
+# Convert array to numpy array
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+# One-hot encoding (https://stackoverflow.com/questions/60868391/how-to-view-class-labels-after-one-hot-encoding-during-training-testing-and-afte)
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+# Use 80% of data for training and 20 for testing 9https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html)
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+# Set parms for random image movement (https://faroit.com/keras-docs/1.2.2/preprocessing/image/)
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=False,
+	fill_mode="nearest")
+
+# Load MobileNetV2 (https://pyimagesearch.com/2019/06/24/change-input-shape-dimensions-for-fine-tuning-with-keras/)
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+
+# Create model head
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+# Put model head on model
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# Freeze training layers so they won't change during first training round
+for layer in baseModel.layers:
+	layer.trainable = False
+
+# Configure model for training with adam optmiser (https://keras.io/api/metrics/)
+print("Compiling model with adam...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# Train head model
+print("Training head...")
+H = model.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(testX, testY),
+	validation_steps=len(testX) // BS,
+	epochs=EPOCHS)
+
+# Make predictions on the testing dataset
+print("Testing model...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+# Prediction index
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# Show final report for results
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+# Save model to disk
+print("Saving...")
+model.save(args["model"], save_format="h5")
